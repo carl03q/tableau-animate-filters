5865091793753fca9742039619ca87cd5832fdc1
Carlos Quintero <carl03q@gmail.com>
2020-01-10 13:47:47 -0500

    Manual

--- animate-filters.js
+++ animate-filters.js
@@ -3,45 +3,49 @@ $(document).ready(function() {
     // Inicializamos Extensions API
     tableau.extensions.initializeAsync().then(function() {
 
-      //  Preguntamos a tableau por las hojas de trabajo.
-      const worksheets = tableau.extensions.dashboardContent.dashboard.worksheets;
+      //Ask tableau for the dashboard
+      const parameter_name = 'P_Region'
+      const filter_name = 'Región'
+      const ws_name = "Ventas por producto"
 
-      // Buscamos una hoja por nombre.
-      var worksheet = worksheets.find(function (sheet) {
-        return sheet.name === "HR Performance";
+      const dashboard = tableau.extensions.dashboardContent.dashboard
+      var worksheet = dashboard.worksheets.find(function (sheet) {
+        return sheet.name === ws_name;
+      });
+      
+      dashboard.findParameterAsync(parameter_name).then(parameter => {
+        console.log("parameters loaded");
+        
+        let p_list = parameter.allowableValues.allowableValues
+        let p_values = []
+        for (let p of p_list) {
+            p_values.push(p.value);
+        }
+
+        current = parameter.currentValue.value
+        filter_index = p_values.indexOf(current)
+        
+        $('#resultBox').text("")
+        //$('#resultBox').text(current)
+
+        return {'list': p_values, '_index': filter_index}
+      }).then(driver => {
+        $('#resultBox').text(driver.list[driver._index])
+
+        setInterval( () => {
+          driver._index += 1 //next index
+          driver._index = driver._index % driver.list.length //cyclical index
+
+          let value = []
+          value.push(driver.list[driver._index])
+          //$('#resultBox').text(String(value))
+
+          worksheet.applyFilterAsync(filter_name, value, 'replace').catch(console.log);
+
+
+        }, 10000);
+
       });
 
-      // Or iterate through the array of worksheets
-      worksheets.forEach(function (worksheet) {
-        //  process each worksheet...
-        console.log(worksheet.name);
-      });
-    
-    // Agregamos evento de selección. Para cuando seleccionemos data desde Tableau lograr una
-    // interacción.
-    let markSelectionHandler = selectionEvent =>{
-        worksheet.getUnderlyingDataAsync().then(dataTable =>{
-            // Obtenemos el valor Employee Number
-            let field = dataTable.columns.find(column => column.fieldName === 'Employee Number');
-            let list = [];
-            for (let row of dataTable.data) {
-                list.push(row[field.index].value);
-            }
-            // Vaciamos la lista del html y la cargamos con imágenes que tengan el
-            // nombre asociado al número de empleado.
-            $('#imageList').empty()
-            for(let emp of list){
-              let image = './assets/'+emp+'.png'
-              let name = emp
-              $('#imageList').append('<li class="list-group-item link-class"><img src="'+image+'" height="200" width="200" class="img-thumbnail" /> '+name+'</span></li>');
-            }
-    
-        });
-    };
-
-    // Agregamos el evento al listener de la extensión.
-    let unregisterHandlerFunction = worksheet.addEventListener(tableau.TableauEventType.MarkSelectionChanged, markSelectionHandler);
-    
-    
   });
 });
